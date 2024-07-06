29017249b36152671854e065fa81b8c1e24b4c07
Abdelrhman shawky <abdelrhman.shawky156@gmail.com>
2024-07-06 04:45:21 +0300

    Creating Floor class
    refactor floor class creation

--- gb/Model/Creation/ElementCreation.cs
+++ gb/Model/Creation/ElementCreation.cs
@@ -1,5 +1,8 @@
 ﻿//using Autodesk.Revit.Creation;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using gb.Model.RevitHelper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +15,70 @@ namespace gb.Model.Creation
     {
         private Document _document;
 
-        SpatialElementBoundaryOptions boundaryOptions = new SpatialElementBoundaryOptions();
+        RevitFilterCollectors _filterCollectors;
 
 
-       public ElementCreation(Document document)
+       public ElementCreation(Document document, RevitFilterCollectors revitFilterCollectors)
         {
             _document = document;
+            _filterCollectors = revitFilterCollectors;
         }
 
-        //IList<IList<BoundarySegment>>
+        /// <summary>
+        /// Collects elements from the document based on the specified category and element type.
+        /// </summary>
+        /// <param room="Room">The built-in category to filter.</param>
+        /// <returns>List of CurveLoops.</returns>
+        private IList<CurveLoop> GetRoomBaseCurve(Room room)
+        {
+            SpatialElementBoundaryOptions boundaryOptions = new SpatialElementBoundaryOptions();
+
+            IList<IList<BoundarySegment>> boundarySegments = room.GetBoundarySegments(boundaryOptions);
+
+
+            IList<CurveLoop> curves = new List<CurveLoop>();
+            foreach (IList<BoundarySegment> segmentList in boundarySegments)
+            {
+                CurveLoop curveLoop = new CurveLoop();
+                foreach (BoundarySegment segment in segmentList)
+                {
+                    Curve curve = segment.GetCurve();
+                    curveLoop.Append(curve);
+                }
+                curves.Add(curveLoop);
+            }
+
+            return curves;
+        }
+
+
+
+        public Floor CreateRoomFloorFromParam(Room room)
+        {
+            IList<CurveLoop> curves =GetRoomBaseCurve(room);
+
+           Parameter floorTypeParam = room.LookupParameter("Floor Finish");
+
+            
+            IList<Element> FloorElment = _filterCollectors.CollectFloorsElements(true);
+
+            Element specificFloorType= FloorElment.FirstOrDefault(e => e.Name ==floorTypeParam.AsValueString());
+
+            var a =  floorTypeParam.Id;
+
+            using (Transaction transaction = new Transaction(_document, "Create Floor"))
+            {
+                transaction.Start();
+
+               Floor myFloor= Floor.Create(_document, curves, specificFloorType.Id, room.LevelId);
+               
+                transaction.Commit();
+                return myFloor;
+            }
+
+            
+        }
+
+
     }
 }
